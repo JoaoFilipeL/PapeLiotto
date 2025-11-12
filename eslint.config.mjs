5d8741680f4318aff7a78f1c110584b04e920090
João Filipe <joaofcliotto@gmail.com>
2025-11-12 00:43:13 -0300

    FINAL

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-empty-interface": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn"
     },
   },
 ];
